b04cad2c4d11a758fd9eed637b88078be7cdc76c
Hye Sun Hong <sunny@gatech.edu>
2016-12-28 13:53:42 +0900

    Create exercise1_2.h

---
+++ ArraysAndStrings/Exercise1_2/exercise1_2.h
@@ -0,0 +1,21 @@
+//
+//  exercise1_2.h
+//  ArraysAndStrings
+//
+//  Created by Hye Sun Hong on 12/28/16.
+//  Copyright © 2016 Hyesun. All rights reserved.
+//
+
+#ifndef exercise1_2_h
+#define exercise1_2_h
+
+class exercise1_2
+{
+public:
+    int run();
+    void reverse(char* str);
+    int main();
+};
+
+#endif /* exercise1_2_h */
+
