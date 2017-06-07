0c2eae53782e922a7002cab89dd795a92b7a7033
Sunny Hong <sunny@gatech.edu>
2017-06-07 00:32:54 -0400

    conflict resolve. minor change

--- 3. Stacks and Queues/InterviewQuestion3_4/InterviewQuestion3_4/main.cpp
+++ 3. Stacks and Queues/InterviewQuestion3_4/InterviewQuestion3_4/main.cpp
@@ -1,6 +1,10 @@
 //
 //  main.cpp
+<<<<<<< HEAD
+//  InterviewQuestion3_4 "Towers of Hanoi" 
+=======
 //  InterviewQuestion3_4
+>>>>>>> origin/master
 //
 //  Created by Hye Sun Hong on 6/6/17.
 //  Copyright © 2017 Hyesun. All rights reserved.
@@ -12,6 +16,31 @@
 
 using namespace std;
 
+<<<<<<< HEAD
+void towersofhanoi(int num,
+                   stack<int> *first,
+                   stack<int> *temp,
+                   stack<int> *dest){
+    
+    if (num ==0){
+        return;
+    }
+    towersofhanoi(num-1, first, dest, temp); // disks= num-1;
+                                             //  towersofhanoi(num, From, temporary, End Tower);
+    (*dest).push( (*first).top() );
+    (*first).pop();
+    towersofhanoi(num-1, temp, first, dest);// towersofhanoi(num, From, temporary, End Tower);
+    
+}
+
+void printstack( stack<int> *obj ){
+    
+    while ( !(*obj).empty() ) {
+        cout<< (*obj).top()<<" ";
+        (*obj).pop();
+    }
+    cout<<endl;
+=======
 //struct node{
 //    int preIndex, value;
 //};
@@ -74,11 +103,27 @@ int towersofhanoi(int towers, int disks){
         return 0;
     }
     
+>>>>>>> origin/master
     
 }
 
 
 int main() {
+<<<<<<< HEAD
+    stack <int> start, st1, end;
+    int disks;
+    disks= 3;
+    for(int i=0; i<disks; i++ ){
+        start.push(disks-i);
+    }
+    
+    towersofhanoi(disks, &start, &st1, &end);
+    cout<<"End stack : ";
+    printstack(&end);
+    
+    return 0;
+}
+=======
     int towers, disks;
     towers = 3; disks=3;
     
@@ -128,3 +173,4 @@ int main() {
 //    int totalSize;
 //    int current;
 //};
+>>>>>>> origin/master
